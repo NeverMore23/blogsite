e711f5a31090703412968370a3a61dd021ee115e
klm123 <klm123@163.com>
2018-08-29 20:28:12 -0700

    --

--- siteconf/settings.py
+++ siteconf/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/tutorial")
+STATIC_ROOT = os.path.join(BASE_DIR, "/DjangoUeditor/static")
 
 # 公共的static文件
 STATICFILES_DIRS = (
